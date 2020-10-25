892ca17a8580d4444eb9e140279de2b1ba46dbb1
李健龙80374687 <lijianlong@cmbchina.com>
2020-10-25 20:56:27 +0800

    test

---
+++ src/test/java/com/jeralee/LearningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeralee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
